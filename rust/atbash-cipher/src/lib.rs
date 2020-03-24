fe4acc02fbecd44e907ab8504750f01a84e5712c
Eduardo Braun <eduardo@rushers.org>
2020-03-23 23:38:55 -0300

    [rust] atbash-cipher: isolate duplicated code

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,15 +1,18 @@
-const ASCII_INVERTER: u8 = 'a' as u8 + 'z' as u8;
+fn ascii_invert_az(c: char) -> Option<char> {
+    const ASCII_INVERTER: u8 = 'a' as u8 + 'z' as u8;
+    match c {
+        'a'..='z' => Some((ASCII_INVERTER - c as u8) as char),
+        '0'..='9' => Some(c),
+        _ => None,
+    }
+}
 
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
     plain
         .to_lowercase()
         .chars()
-        .filter_map(|c| match c {
-            'a'..='z' => Some((ASCII_INVERTER - c as u8) as char),
-            '0'..='9' => Some(c),
-            _ => None,
-        })
+        .filter_map(|c| ascii_invert_az(c))
         .collect::<Vec<char>>()
         .chunks(5)
         .map(|chunk| chunk.iter().collect::<String>())
@@ -21,10 +24,6 @@ pub fn encode(plain: &str) -> String {
 pub fn decode(cipher: &str) -> String {
     cipher
         .chars()
-        .filter_map(|c| match c {
-            'a'..='z' => Some((ASCII_INVERTER - c as u8) as char),
-            '0'..='9' => Some(c),
-            _ => None,
-        })
+        .filter_map(|c| ascii_invert_az(c))
         .collect::<String>()
 }
